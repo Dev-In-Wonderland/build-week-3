47b06bcec8c0320c2a06579321f83b3136af31e4
Ele-Ele <eleonora98proietti@gmail.com>
2023-03-01 13:09:32 +0100

    Add fetch post

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,75 @@
-import { Col, Row, Form } from "react-bootstrap"
-import { AiFillLinkedin, AiFillMessage } from "react-icons/ai"
+import { Col, Row, Form } from "react-bootstrap";
+import { AiFillLinkedin, AiFillMessage } from "react-icons/ai";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { MdHome, MdWork, MdNotifications } from "react-icons/md";
 import { BsFillPeopleFill, BsFillPersonFill } from "react-icons/bs";
 import { CgMenuGridR } from "react-icons/cg";
-
-
+import { Link } from "react-bootstrap/lib/Navbar";
 
 const Navbar = () => {
-    return(
-        <Row className="w-100 navbar-navigation d-flex justify-content-center">
-        
-        <Col className="nav-col d-flex justify-content-center align-items-center" xs={10}>
-            <div className="input-search">
-                <AiFillLinkedin className="linkedin-logo"/>
-                <Form className="d-flex form d-none d-lg-block">
-                    <Form.Control
-                        type="search"
-                        placeholder="Cerca"
-                        className="me-2 form-ricerca"
-                        aria-label="Search"
-                        
-                    />
-                
-                </Form>
-            </div>
-            
-                <ul className="ul-nav">
-                    <li>
-                        < MdHome className="i-nav"/>
-                        <span className="d-none d-lg-block">Home</span>
-                    </li>
-                    <li>
-                        < BsFillPeopleFill className="i-nav"/>
-                        <span className="d-none d-lg-block">Rete</span>
-                    </li>
-                    <li>
-                        < MdWork className="i-nav"/>
-                        <span className="d-none d-lg-block">Lavoro</span>
-                    </li>
-                    <li>
-                        < AiFillMessage className="i-nav"/>
-                        <span className="d-none d-lg-block">Messaggistica</span>
-                    </li>
-                    <li>
-                        < MdNotifications className="i-nav"/>
-                        <span className="d-none d-lg-block">Notifiche</span>
-                    </li>
-                    <li >
-                        < BsFillPersonFill className="i-nav"/>
-                        <span className="d-none d-lg-block">Tu <IoMdArrowDropdown className="freccia-dropdown"/></span>
-                    </li>
-                    </ul>
-                    <ul className="ul-nav">
-                        <li>
-                            < CgMenuGridR className="i-nav"/>
-                            <span className="d-none d-lg-block">Lavoro <IoMdArrowDropdown className="freccia-dropdown"/></span>
-                        </li>
-                        <li >
-                            <a href="a">Prova Premium</a>
-                        </li>
-                    </ul>                    
-        </Col>
-        
-    </Row>
-    )
-    
-}
+  return (
+    <Row className="w-100 navbar-navigation d-flex justify-content-center">
+      <Col
+        className="nav-col d-flex justify-content-center align-items-center"
+        xs={10}
+      >
+        <div className="input-search">
+          <AiFillLinkedin className="linkedin-logo" />
+          <Form className="d-flex form d-none d-lg-block">
+            <Form.Control
+              type="search"
+              placeholder="Cerca"
+              className="me-2 form-ricerca"
+              aria-label="Search"
+            />
+          </Form>
+        </div>
 
-export default Navbar
+        <ul className="ul-nav">
+          <li>
+            <MdHome className="i-nav" />
+            <span className="d-none d-lg-block">Home</span>
+          </li>
+          <li>
+            <BsFillPeopleFill className="i-nav" />
+            <span className="d-none d-lg-block">Rete</span>
+          </li>
+          <li>
+            <MdWork className="i-nav" />
+            <span className="d-none d-lg-block">Lavoro</span>
+          </li>
+          <li>
+            <AiFillMessage className="i-nav" />
+            <span className="d-none d-lg-block">Messaggistica</span>
+          </li>
+          <li>
+            <MdNotifications className="i-nav" />
+            <span className="d-none d-lg-block">Notifiche</span>
+          </li>
+          <li>
+            <BsFillPersonFill className="i-nav" />
+            <Link to="/Me">
+              {" "}
+              <span className="d-none d-lg-block">
+                Tu <IoMdArrowDropdown className="freccia-dropdown" />
+              </span>
+            </Link>
+          </li>
+        </ul>
+        <ul className="ul-nav">
+          <li>
+            <CgMenuGridR className="i-nav" />
+            <span className="d-none d-lg-block">
+              Lavoro <IoMdArrowDropdown className="freccia-dropdown" />
+            </span>
+          </li>
+          <li>
+            <a href="a">Prova Premium</a>
+          </li>
+        </ul>
+      </Col>
+    </Row>
+  );
+};
+
+export default Navbar;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -20,9 +20,12 @@ const Post = () => {
     try {
       const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZlMjc4ODU3OWM2MzAwMTM3Y2Y4YzMiLCJpYXQiOjE2Nzc2MDA2NDksImV4cCI6MTY3ODgxMDI0OX0.EHJrg1AvvFDXzLcMgar_TjwQaMNKVN_tbGsUktYNUHQ";
-      const response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       if (response.ok) {
         const data = await response.json();
         setPost(data);
@@ -46,30 +49,28 @@ const Post = () => {
   return (
     <>
       {post?.map((e, i) => (
-        <Container>
-          <Row className="cols-3">
-            <Col className="col-3">col1</Col>
-            <Col className="col-6">
-              <Card.Body className="d-flex border border-danger">
-                <div>
-                  <Card.Title className="sopra">
-                    <p className="m-0  p-0">{e.username}</p>{" "}
-                    <button className="btn border border-none">
-                      <Example />
-                    </button>
-                    <p className="m-0  p-0">{e.text}</p>
-                  </Card.Title>
-                </div>
-                {/*<img
+        <Row className="cols-3">
+          <Col className="col-3">col1</Col>
+          <Col className="col-6">
+            <Card.Body className="d-flex border border-danger rounded m-2 bg-light-subtle">
+              <div>
+                <Card.Title className="sopra">
+                  <p className="m-0  p-0">{e.username}</p>{" "}
+                  <button className="btn border border-none bg-secondary">
+                    <Example />
+                  </button>
+                  <p className="m-0  p-0">{e.text}</p>
+                </Card.Title>
+              </div>
+              {/*<img
           src={e.image}
           className="profilo"
           alt="immagine dell'esperienza"
   />*/}
-              </Card.Body>
-            </Col>
-            <Col className="col-3">col2</Col>
-          </Row>
-        </Container>
+            </Card.Body>
+          </Col>
+          <Col className="col-3">col2</Col>
+        </Row>
       ))}
     </>
   );
