42a03a1164491fdf8834089f0dc7ffe069423cb7
OmarCovolo <OmarCovolo@users.noreply.github.com>
2023-03-06 12:55:05 +0100

    szxdcfgvhbjnkl,

--- src/components/deletePost.jsx
+++ src/components/deletePost.jsx
@@ -4,12 +4,12 @@ import { useDispatch } from "react-redux";
 
 const RemovePostAction = (i) => ({ type: "REMOVE_POST", payload: i });
 
-function DeletePost() {
+function DeletePost(props) {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZlMjc4ODU3OWM2MzAwMTM3Y2Y4YzMiLCJpYXQiOjE2Nzc2MDA2NDksImV4cCI6MTY3ODgxMDI0OX0.EHJrg1AvvFDXzLcMgar_TjwQaMNKVN_tbGsUktYNUHQ";
 
   // const dispatch = useDispatch();
-  const fetchDelete = async (props) => {
+  const fetchDelete = async () => {
     try {
       const response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${props.id}`, {
         method: "DELETE",
