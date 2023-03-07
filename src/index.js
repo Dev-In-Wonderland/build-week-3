8e14980c8132b844e784889900594ffca5d1ce1b
OmarCovolo <OmarCovolo@users.noreply.github.com>
2023-03-07 17:03:31 +0100

    DC

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './redux';
+import { PersistGate } from "redux-persist/integration/react";
+import { store,persistor } from './redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store = {store}>
+  <Provider store={store}>
+  <PersistGate persistor={persistor}>
     <App />
-  </Provider>
+  </PersistGate>
+</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+
 import { profileReducer } from "./reducers/profile";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
-export const store = configureStore ({reducer: profileReducer})
 
 
+const persistConfig = {
+  key: "root",
+  storage, 
+  transforms: [
+    encryptTransform({
+      secretKey: "SUPER_SECRET_KEY",
+      onError: function (error) {
+        console.log(error)
+      },
+    }),
+  ],
+
+};
 
 
+const rootReducer = combineReducers({
+  profile:profileReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// configureStore ha bisogno della struttura del nostro store/stato globale, come parametro principale (quindi un reducer)
+export const store = configureStore({
+  // reducer
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    }) // può esserci solo un valore per reducer nello store
+});
+
+export const persistor = persistStore(store);
